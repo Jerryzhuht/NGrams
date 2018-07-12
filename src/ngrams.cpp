8a3d7e111312cd1cb812cce7dddc242f4e614924
Horan <zhanghaoranwww@gmail.com>
2018-07-12 00:45:26 -0700

    initial commit

---
+++ src/ngrams.cpp
@@ -0,0 +1,84 @@
+// This is the CPP file you will edit and turn in. (TODO: Remove this comment!)
+
+#include <iostream>
+#include "console.h"
+#include <string>
+#include "lexicon.h"
+#include "strlib.h"
+#include "simpio.h"
+#include "vector.h"
+#include "queue.h"
+#include "filelib.h"
+using namespace std;
+
+// when reading a file, it has to be When reading the input file, the idea is to keep a window of N-1 words at all times, and as you read each word from the file, discard the first word from your window and append the new word.
+// The matter matters ('are you' is different from 'you are')
+//
+//Prototypes
+void welcome();
+void readFile(Vector<string> grams_map, Vector<string> grams_window, ifstream&infile, int N);
+
+
+int main() {
+    // Print the welcoming text
+    welcome();
+    // Open the file and reprompt the user if the file is invalid
+    ifstream stream;
+    string prompt = "Input file name?";
+    string reprompt = "Unable to open that file. Try again!";
+    promptUserForFile(stream, prompt, reprompt);
+    // How can i store the file, what ADT should I use @@
+    //read the input file
+    // Assign the literature to a variable called work
+    Lexicon lex(filename);
+    //work = Set<string>stream;
+
+    // Prompt the user for integer input "N"
+    int N = stringToInteger(getLine("Value of N? "));
+    int Num = stringToInteger(getLine("# of random words to generate (0 to quit)."));
+    //Call the readFile function@@
+
+
+
+
+    cout << "Exiting." << endl;
+    return 0;
+}
+
+
+void welcome (){
+    cout << "Welcome to CS 106B/X Random Writer ('N-Grams')!" << endl;
+    cout << "This program generates random text based on a document." << endl;
+    cout << "Give me an input file and an 'N' value for groups" << endl;
+    cout << "of words, and I'll create random text for you." << endl;
+    cout << endl;
+
+}
+
+void readFile(Vector<string> grams_map, Vector<string> grams_window, ifstream&infile, int N){
+    // Keep a window of N-1 words at all time
+
+    string filename = getLine("Filename");
+
+    promptUserForFile(infile, "ladygaga.txt");
+
+    grams_window = Vector<string>;
+    //grams_map = Map<Queue<Stack<string>>, queue<stack>>;
+    grams_map = Map<Vector<string>, Vector<string>>;
+    Queue<
+    //How should I call the literature here@@
+    //string (line);
+
+    //while@@
+
+    // keep a counter of the random words generated
+    <int>sum_words = 0
+    //iterating through all the lines
+
+
+
+
+
+
+
+}
