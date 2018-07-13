87f17e2788c61deb5c768c39065e301d755529d3
Jerry Zhu <jerryzhuht@gmail.com>
2018-07-13 11:16:38 -0700

    readFile and wordGen complete

--- src/ngrams.cpp
+++ src/ngrams.cpp
@@ -1,5 +1,3 @@
-// This is the CPP file you will edit and turn in. (TODO: Remove this comment!)
-
 #include <iostream>
 #include "console.h"
 #include <string>
@@ -25,17 +23,26 @@ int main() {
     cout << endl;
 
     Map<Queue<string>,Vector<string>> dictionary = readFile();
+//    test
+//    Vector<Queue<string>> allKeys = dictionary.keys();
+//    int keyNum = allKeys.size();
+//    for (int i=0; i<keyNum; i++){
+//        cout << allKeys[i]<<" : "<<dictionary[allKeys[i]]<<endl;
+//    }
+
     while(true){
         int n = getInteger("# of random words to generate (0 to quit):");
         if (n == 0){
             break;
         }
+
         Queue<string> sentence = wordGen(n,dictionary);
         while (!sentence.isEmpty()){
             string word = sentence.dequeue();
-            cout << word << "";
-            cout << endl;
+            cout << word << " ";
         }
+        cout << endl;
+        cout << endl;
     }
 
     cout << "Exiting." << endl;
@@ -47,38 +54,32 @@ int main() {
 Map<Queue<string>,Vector<string>> readFile(){
     ifstream infile;
     promptUserForFile(infile, "Input file name");
-
     int Ngram = getInteger("Value of N?");
 
     Map<Queue<string>,Vector<string>> dictionary;
-
     cout<< endl;
-    //@@How can I know the position of N and n in the programming
-    //Create an empty map to store the key and value
-    //Map<Vector<string>, Vector<string>> map;
     Queue<string> passage;
 
     string word;
     while(infile >> word){
         passage.enqueue(word);
     }
+
     int wordCount = passage.size();
     Queue<string> window;
-    // from passage to window
+
+    // init passage and window
+    for (int j=1; j<Ngram; j++){
+        string keyWord = passage.dequeue();
+        window.enqueue(keyWord);
+        passage.enqueue(keyWord);
+    }
+
     for (int i=0; i<wordCount;i++){
-        for (int j=1; j<Ngram; j++){
-            string keyWord = passage.dequeue();
-            window.enqueue(keyWord);
-            passage.enqueue(keyWord);
-        }
         string valueWord = passage.dequeue();
         // from window to key queue
         Queue<string> keyQueue = window;
-//        Vector<string> keyVec;
-//        while (!getWord.isEmpty()){
-//            keyVec.add(getWord.dequeue());
-//        }
-//        Vector<string> valueVec;
+
         // check if key exist
         Vector<string> valueVec;
         if (dictionary.containsKey(keyQueue)){
@@ -102,31 +103,25 @@ Queue<string> wordGen(int n, Map<Queue<string>,Vector<string>> &dictionary){
     Queue<string> sentence;
     // all the keys in dictionary
     Vector<Queue<string>> availableKeys = dictionary.keys();
+    // select first word
     int rand1 = randomInteger(0, availableKeys.size()-1);
-    // first N words
-    Queue<string> startQueue = availableKeys[rand1];
+    Queue<string> windows = availableKeys[rand1];
 
-    // init windows
-    Queue<string> windows;
-    windows = startQueue;
-    sentence = startQueue;
-//    while (!startQueue.isEmpty()){
-//        windows.enqueue(startVec[i]);
-//        sentence.enqueue(startVec[i]);
-//    }
+    // init sentence
+    sentence = windows;
+    int senLength = sentence.size();
 
-    while(sentence.size() < n ){
+    while(senLength < n ){
         // vector for next word
-        Vector<string> nextVec = dictionary[startQueue];
+        Vector<string> nextVec = dictionary[windows];
         int rand2 = randomInteger(0, nextVec.size()-1);
         string nextWord = nextVec[rand2];
         // add into sentence
         sentence.enqueue(nextWord);
+        senLength = sentence.size();
         // adjust windows
         windows.enqueue(nextWord);
         windows.dequeue();
-        // adjust startQueue
-        startQueue.dequeue();
     }
     return sentence;
 }
